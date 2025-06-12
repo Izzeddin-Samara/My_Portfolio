7a4aa5ee8d4d2d1582e5a69efe47747984b6e7b5
Izzsamara <izzidinsamara@gmail.com>
2025-06-12 20:37:07 +0300

    Import and use the HeroSection component in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
   
   return (
     <main className="dark:bg-gray-900">
       
-     
+     <HeroSection/>
     </main>
   );
 }
