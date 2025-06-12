8c95bc571466f8e07edf4b923c5c74413b0e3959
Izzsamara <izzidinsamara@gmail.com>
2025-06-12 20:34:50 +0300

    Remove import for Image and DarkThemeToggle and update the style for main in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
-import { DarkThemeToggle } from "flowbite-react";
-import Image from "next/image";
-
 export default function Home() {
   
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-white px-4 py-24 dark:bg-gray-900">
-      <div className="absolute top-4 right-4">
-        <DarkThemeToggle />
-      </div>
-
+    <main className="dark:bg-gray-900">
+      
      
     </main>
   );
