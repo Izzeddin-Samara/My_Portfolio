48701a553cd37995d67a69ca2d99db474eff21bc
Izzsamara <izzidinsamara@gmail.com>
2025-06-12 13:56:51 +0300

    Add hamburger menu for mobile screen in Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,16 @@
 "use client";
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import Link from "next/link";
 import { DarkThemeToggle } from "flowbite-react";
 import { usePathname } from "next/navigation";
 
-
 export default function Navbar() {
-  
+  const [isOpen, setIsopen] = useState(false);
+
+  const toggle = () => {
+    setIsopen(!isOpen);
+  };
 
   const pathname = usePathname();
 
@@ -81,7 +86,82 @@ export default function Navbar() {
               <DarkThemeToggle />
             </li>
           </ul>
+          {/* Hamburger Icon (only visible on mobile) */}
+          <button onClick={toggle} className="m-4 md:hidden dark:text-white">
+            {isOpen ? <FaTimes size={30} /> : <FaBars size={30} />}
+          </button>
         </div>
+
+        {/* Mobile Menu (Hidden by default, shown when `isOpen` is true) */}
+        <ul
+          className={`space-y-4 bg-gray-300 p-4 text-center md:hidden dark:bg-gray-700 dark:text-white ${
+            isOpen ? "block" : "hidden"
+          }`}
+        >
+          <li>
+            <Link
+              href="/"
+              className={`text-lg hover:text-purple-700 md:text-xl dark:hover:text-purple-500 ${
+                pathname === "/"
+                  ? "font-bold text-purple-700 dark:text-purple-500"
+                  : ""
+              }`}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/about"
+              className={`text-lg hover:text-purple-700 md:text-xl dark:hover:text-purple-500 ${
+                pathname === "/about"
+                  ? "font-bold text-purple-700 dark:text-purple-500"
+                  : ""
+              }`}
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/contact"
+              className={`text-lg hover:text-purple-700 md:text-xl dark:hover:text-purple-500 ${
+                pathname === "/contact"
+                  ? "font-bold text-purple-700 dark:text-purple-500"
+                  : ""
+              }`}
+            >
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/skills"
+              className={`text-lg hover:text-purple-700 md:text-xl dark:hover:text-purple-500 ${
+                pathname === "/skills"
+                  ? "font-bold text-purple-700 dark:text-purple-500"
+                  : ""
+              }`}
+            >
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/projects"
+              className={`text-lg hover:text-purple-700 md:text-xl dark:hover:text-purple-500 ${
+                pathname === "/projects"
+                  ? "font-bold text-purple-700 dark:text-purple-500"
+                  : ""
+              }`}
+            >
+              Projects
+            </Link>
+          </li>
+          <li>
+            <DarkThemeToggle />
+          </li>
+        </ul>
       </nav>
     </>
   );
