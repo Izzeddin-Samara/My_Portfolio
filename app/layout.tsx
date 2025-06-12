77137afd0d514c77b4bbf878d52abed890b62d63
Izzsamara <izzidinsamara@gmail.com>
2025-06-12 14:55:23 +0300

    Remove geistSans and geistMono from layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,15 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Flowbite React Template Next.js",
@@ -30,7 +22,6 @@ export default function RootLayout({
         <ThemeModeScript />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar/>
         {children}
