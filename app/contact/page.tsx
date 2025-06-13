972dfd22bd33c4ca6e2ce2baede8d4aa1a108274
Izzsamara <izzidinsamara@gmail.com>
2025-06-13 12:21:33 +0300

    Add Contact body section to Contact Page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -20,6 +20,104 @@ export default function Contact() {
               </p>
             </div>
           </div>
+
+          {/* Contact body*/}
+          <div className="w-full">
+            <div className="mx-auto h-auto max-w-4xl p-4">
+              <div className="mx-auto grid grid-cols-1 gap-8 md:grid-cols-1 lg:grid-cols-1">
+                {/* Contact Form*/}
+                <div className="rounded-lg bg-gray-100 p-4 shadow-xl dark:bg-gray-300">
+                  <form className="mx-auto w-3/4 space-y-6 p-4 text-center">
+                    <div>
+                      <label className="text-xl">Name</label>
+
+                      <input
+                        className="text-md md:text-md mt-4 w-full rounded-lg border border-gray-400 p-4 outline-none focus:ring-3 focus:ring-purple-700 md:p-4 dark:outline-none"
+                        type="text"
+                        name="name"
+                        placeholder="eg: John Doe"
+                      />
+                    </div>
+                    <div>
+                      <label className="text-xl">Email</label>
+
+                      <input
+                        className="text-md md:text-md mt-4 w-full rounded-lg border border-gray-400 p-4 outline-none focus:ring-3 focus:ring-purple-700 md:p-4"
+                        type="text"
+                        name="email"
+                        placeholder="eg: johndoe@gmail.com"
+                      />
+                    </div>
+                    <div>
+                      <label className="text-xl">Message</label>
+
+                      <textarea
+                        id="message"
+                        name="message"
+                        className="text-md mt-2 h-48 w-full resize-none rounded-lg border border-gray-400 bg-gray-100 p-3 outline-none focus:ring-3 focus:ring-purple-700 dark:bg-gray-300"
+                        placeholder="Write your message here"
+                      ></textarea>
+                    </div>
+
+                    <button
+                      type="submit"
+                      className="w-full cursor-pointer rounded-lg bg-purple-700 p-4 text-xl font-bold text-white hover:bg-purple-800 focus:ring-4 focus:ring-purple-300"
+                    >
+                      Send Message
+                    </button>
+                  </form>
+                </div>
+                {/* Contact Information*/}
+                <div className="mt-20 flex justify-center gap-30 space-y-16">
+                  <div className="text-center">
+                    <FaEnvelope
+                      className="mx-auto rounded-lg text-purple-700"
+                      size={60}
+                    />
+                    <h1 className="font-bold dark:text-white">Email</h1>
+                    <p className="mt-4 font-bold text-purple-700">
+                      <a
+                        href="mailto:izzidinsamara@gmail.com"
+                        className="underline hover:no-underline"
+                      >
+                        izzidinsamara@gmail.com
+                      </a>
+                    </p>
+                  </div>
+                  <div className="text-center">
+                    <FaLinkedin
+                      className="mx-auto rounded-lg p-2 text-purple-700"
+                      size={60}
+                    />
+                    <h1 className="font-bold dark:text-white">Linkedin</h1>
+                    <p className="mt-4 font-bold text-purple-700">
+                      <a
+                        href="https://www.linkedin.com/in/izzeddin-samara"
+                        className="underline hover:no-underline"
+                      >
+                        izzeddin-samara
+                      </a>
+                    </p>
+                  </div>
+                  <div className="text-center">
+                    <FaWhatsapp
+                      className="mx-auto rounded-lg p-2 text-purple-700"
+                      size={60}
+                    />
+                    <h1 className="font-bold dark:text-white">Whatsapp</h1>
+                    <p className="mt-4 font-bold text-purple-700">
+                      <a
+                        href="https://api.whatsapp.com/send/?phone=00972595484832&text&type=phone_number&app_absent=0"
+                        className="underline hover:no-underline"
+                      >
+                        +972 59-548-4832
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
