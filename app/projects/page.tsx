8ee0ccc5c2487b094ef404a3d7463807d59f79cc
Izzsamara <izzidinsamara@gmail.com>
2025-06-13 20:45:21 +0300

    Add Projects page with introduction section

---
+++ app/projects/page.tsx
@@ -0,0 +1,26 @@
+
+
+export default function Projects() {
+  
+  return (
+    <>
+      <div className="dark:bg-gray-800">
+        <div className="mx-auto min-h-screen max-w-7xl py-30">
+          <div className="text-center">
+            <h1 className=" text-5xl font-bold text-purple-700 dark:text-purple-500">
+              Projects
+            </h1>
+            <p className="text-justify mt-10 text-xl w-[90%] mx-auto dark:text-white" >
+              Here you'll find a selection of the applications and websites I've
+              built. Each project showcases my skills with modern technologies
+              and my passion for creating clean, user-friendly experiences. Feel
+              free to explore, try the live demos, and get a glimpse of what I
+              can do
+            </p>
+          </div>
+          
+        </div>
+      </div>
+    </>
+  );
+}
