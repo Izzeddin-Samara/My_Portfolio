eb137224ec47314e78e0b9cca3ee9a2d183cef57
Izzsamara <izzidinsamara@gmail.com>
2025-06-14 19:35:04 +0300

    Import and use 'WhatDrivesMe' component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import HeroSection from "@/components/HeroSection";
+import WhatDrivesMe from "@/components/WhatDrivesMe";
 
 export default function Home() {
   
@@ -6,6 +7,7 @@ export default function Home() {
     <main className="dark:bg-gray-900">
       
      <HeroSection/>
+     <WhatDrivesMe/>
     </main>
   );
 }
