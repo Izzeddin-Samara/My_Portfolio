5ac07712a86387079b96828c47f7a69ae784319b
Izzsamara <izzidinsamara@gmail.com>
2025-06-14 14:31:54 +0300

    Update text size and add leading loose for 'p' tag in About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,7 +9,7 @@ export default function About() {
             <h1 className="mt-15 mb-10 text-center text-2xl font-bold text-purple-700 md:text-4xl dark:text-purple-500">
               About me
             </h1>
-            <p className="text-md mb-4 text-center text-justify md:text-lg dark:text-white">
+            <p className="text-md mb-4 text-center text-justify md:text-xl leading-loose dark:text-white">
               I'm Izzeddin, a{" "}
               <span className="font-bold text-purple-700 dark:text-purple-400">
                 Frontend Developer
@@ -23,11 +23,11 @@ export default function About() {
               interfaces and turning concepts into interactive web experiences.
             </p>
           </div>
-          <div className="text-md px-6 py-20 md:text-lg dark:text-white">
+          <div className="text-md px-6 py-20 md:text-xl dark:text-white ">
             <h1 className="text-center text-2xl font-bold text-purple-700 md:text-4xl dark:text-purple-500">
               Why I Entered Web Development?
             </h1>
-            <p className="mt-8 mb-4 text-center text-justify">
+            <p className="mt-8 mb-4 text-center text-justify leading-loose ">
               My journey into web development started from a growing curiosity
               about how websites are built. I come from a different background —
               I studied accounting and worked in sales for several years. But
@@ -36,14 +36,14 @@ export default function About() {
               of building something from scratch that people can use online
               really appealed to me.
             </p>
-            <p className="text- mt- mb-4 text-center text-justify">
+            <p className="text- mt- mb-4 text-center text-justify leading-loose">
               I joined a full-stack development bootcamp where I built real
               projects using HTML, CSS, JavaScript and backend tools like
               Django. Through that experience, I discovered how much I enjoy
               frontend development — especially the process of turning ideas and
               designs into responsive, user-friendly interfaces
             </p>
-            <p className="text- mt- mb-4 text-center text-justify">
+            <p className="text- mt- mb-4 text-center text-justify leading-loose">
               Now, I focus mostly on technologies like React, Next.js,
               TypeScript, and Tailwind CSS. I enjoy creating clean, modern, and
               accessible web experiences, and I’m continuously learning to
@@ -52,25 +52,25 @@ export default function About() {
               I chose this path
             </p>
           </div>
-          <div className="text-md px-6 md:py-5 md:text-lg dark:text-white">
+          <div className="text-md px-6 md:py-5 md:text-xl dark:text-white">
             <h1 className="text-center text-2xl font-bold text-purple-700 md:text-4xl dark:text-purple-500">
               My Long-Term Goals
             </h1>
-            <p className="mb- mt-8 text-center text-justify">
+            <p className="mb- mt-8 text-center text-justify leading-loose">
               In the long run, I want to become a highly skilled full-stack
               developer who can build complete, scalable web applications from
               start to finish. I aim to deepen my knowledge of modern frontend
               frameworks like React and Next.js, while also expanding my backend
               skills with Node.js and databases.
             </p>
-            <p className="mb- mt-4 text-center text-justify">
+            <p className="mb- mt-4 text-center text-justify leading-loose">
               I’m passionate about creating clean, efficient, and accessible
               user experiences, and I hope to work on projects that make a
               positive impact on users’ lives. Eventually, I want to contribute
               to open-source projects, collaborate with talented teams, and
               maybe even lead development projects.
             </p>
-            <p className="mb- mt-4 text-center text-justify">
+            <p className="mb- mt-4 text-center text-justify leading-loose">
               Continuous learning is very important to me — I plan to stay
               up-to-date with new technologies, improve my problem-solving
               skills, and maybe explore areas like UI/UX design and cloud
@@ -86,7 +86,7 @@ export default function About() {
               Looking for a passionate frontend developer who's always learning
               and eager to bring ideas to life?
             </h1>
-            <p className="mt-8 text-center text-sm md:text-lg dark:text-white">
+            <p className="mt-8 text-center text-sm md:text-xl dark:text-white">
               I'd love to connect and explore opportunities where I can
               contribute, grow, and build great things together.
             </p>
