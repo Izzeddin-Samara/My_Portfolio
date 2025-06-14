463690052afeed15405433da84580266edc53132
Izzsamara <izzidinsamara@gmail.com>
2025-06-14 11:14:42 +0300

    Add projects array for Projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,64 @@
+import { SiTypescript } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { SiExpress } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
+import { HiUsers } from "react-icons/hi2";
+import { IoIosRocket } from "react-icons/io";
 
 
 export default function Projects() {
-  
+  const projects = [
+    {
+      name: "TeamBase",
+      icon: <HiUsers size={50} />,
+      overview:
+        "TeamBase is a web application for managing team member information. It includes login and registration, and lets users add, edit, delete, and search employee records.",
+      screenshot: "Home.jpg",
+      features: [
+        "User authentication (login and registration)",
+        "Add new employee records",
+        "Edit existing employee information",
+        "Delete employees",
+        "Search employees data",
+        "Contact form powered by Mailjet",
+        "Responsive and clean user interface",
+      ],
+      techstack: [
+        <FaReact size={60} color="#007acc" />,
+        <SiTypescript size={60} color="#007acc" />,
+        <RiTailwindCssFill size={60} color="#06b6d4" />,
+        <SiExpress size={60} color="black" />,
+        <SiMongodb size={60} color="#439934" />,
+      ],
+      liveDemo: "https://team-base-dd23.vercel.app/",
+      githubrepo: "https://github.com/Izzeddin-Samara/TeamBase",
+    },
+    {
+      name: "TechNova",
+      icon: <IoIosRocket size={50} />,
+
+      overview:
+        "Technova is a modern software services website built with React and Tailwind CSS, offering a range of digital solutions including custom software development, cloud services, UI/UX design, cybersecurity, IT consulting, and digital transformation. It showcases company's services in a clean, responsive, and professional layout",
+      screenshot: "TechNova.jpeg",
+      features: [
+        "Modern, fully responsive design — optimized for all screen sizes",
+        "Home page — features a Pricing section and client Testimonials",
+        " About Us page — includes Team members and Partnerships",
+        " Services page — showcases core offerings with sleek icons",
+        "Contact Us page — simple form for easy communication",
+        "Careers page — includes an application form with CV upload",
+        "Clean, intuitive layout — with a modern Navbar and Footer",
+      ],
+      techstack: [
+        <FaReact size={60} color="#007acc" />,
+        <SiTypescript size={60} color="#007acc" />,
+        <RiTailwindCssFill size={60} color="#06b6d4" />,
+      ],
+      liveDemo: "https://tech-nova-tawny.vercel.app/",
+      githubrepo: "https://github.com/Izzeddin-Samara/TechNova",
+    },
+  ];
   return (
     <>
       <div className="dark:bg-gray-800">
@@ -18,7 +75,6 @@ export default function Projects() {
               can do
             </p>
           </div>
-          
         </div>
       </div>
     </>
