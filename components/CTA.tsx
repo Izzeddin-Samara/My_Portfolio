d4e6cc0e7b5ba3d1f19e150ee4d1f6893429566c
Izzsamara <izzidinsamara@gmail.com>
2025-06-18 18:55:25 +0300

    Update py and text size for h1 in CTA component

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function CTA() {
   return (
     <>
-      <div className="px-6 py-20 text-center mx-auto">
-        <h1 className="text-center md:w-3/4 mx-auto md:text-xl text-lg text-justify font-bold md:text-xl  dark:text-white">
+      <div className="px-6 py-30 text-center mx-auto">
+        <h1 className="text-center md:w-3/4 mx-auto md:text-2xl text-lg text-justify font-bold md:text-xl  dark:text-white">
           Available for hire and always open to exciting opportunities. If
           you&rsquo;re looking for a frontend developer who cares about clean code,
           beautiful design, and real user experience — let&rsquo;s talk
