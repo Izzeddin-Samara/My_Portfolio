aaccd21d5eb1d6d044d7be85e313b28bdc05ebe8
Izzsamara <izzidinsamara@gmail.com>
2025-06-18 14:33:45 +0300

    Update padding and text size in 'WhatDrivesMe' component

--- components/WhatDrivesMe.tsx
+++ components/WhatDrivesMe.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 export default function WhatDrivesMe() {
   return (
     <>
-      <div className="h-auto p-8">
+      <div className="h-auto p-4">
         <div className="mx-auto max-w-7xl text-center">
           <h1 className="text-4xl font-bold text-purple-700 dark:text-purple-500">
             What Drives me?
           </h1>
-          <p className="mt-8 text-justify text-xl leading-loose dark:text-white">
+          <p className="mt-8 text-justify text-lg leading-loose md:text-xl dark:text-white">
             My path to web development might not be traditional — I started with
             accounting and sales, but curiosity and a love for technology led me
             here. I believe that every website should be more than just code; it
@@ -18,10 +18,15 @@ export default function WhatDrivesMe() {
             eager to explore new tools, sharpen my craft, and turn ideas into
             real, user-friendly solutions.
           </p>
-         <p className="mt-8 text-xl leading-loose dark:text-white mb-10">
-              Want to know more about my journey and what I’m working toward? <Link className="border-b-4 dark:text-purple-500 hover:border-transparent  text-md transition-all border-purple-700 dark:border-purple-500 dark:hover:border-transparent text-purple-700  ml-2  font-bold" href="/about">Learn more</Link>
+            <p className="mt-8 mb-10 text-xl leading-loose dark:text-white">
+              Want to know more about my journey and what I’m working toward?{" "}
+              <Link
+                className="text-md ml-2 border-b-4 border-purple-700 font-bold text-purple-700 transition-all hover:border-transparent dark:border-purple-500 dark:text-purple-500 dark:hover:border-transparent"
+                href="/about"
+              >
+                Learn more
+              </Link>
             </p>
-            
         </div>
       </div>
     </>
